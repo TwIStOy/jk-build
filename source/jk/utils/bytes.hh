16c59ba4ef36be5fa9b7f39472fff11ee9a45d73
Hawtian Wang <twistoy.wang@gmail.com>
2020-08-03 21:29:03 +0800

    🚧 (*): Fix spedd

--- source/jk/utils/bytes.hh
+++ source/jk/utils/bytes.hh
@@ -14,11 +14,11 @@ inline std::string BytesCount(uint64_t x) {
   if (x < 1024) {
     return fmt::format("{}", x);
   } else if (x < 1024 * 1024) {
-    return fmt::format("{:.2}Kb", x / 1024.0);
+    return fmt::format("{:.2f}Kb", x / 1024.0);
   } else if (x < 1024 * 1024 * 1024) {
-    return fmt::format("{:.2}Mb", x / 1024.0 / 1024.0);
+    return fmt::format("{:.2f}Mb", x / 1024.0 / 1024.0);
   }
-  return fmt::format("{:.2}Gb", x / 1024.0 / 1024.0 / 1024.0);
+  return fmt::format("{:.2f}Gb", x / 1024.0 / 1024.0 / 1024.0);
 }
 
 }  // namespace jk::utils
