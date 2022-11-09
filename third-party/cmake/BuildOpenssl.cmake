d1cd4c860c6399e561de041fca44c4763faea7f9
Hawtian Wang <twistoy.wang@gmail.com>
2022-11-09 21:46:37 +0800

    fix openssl on m1 mac

--- third-party/cmake/BuildOpenssl.cmake
+++ third-party/cmake/BuildOpenssl.cmake
@@ -1,6 +1,17 @@
+if (${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
+  if (${CMAKE_APPLE_SILICON_PROCESSOR} MATCHES "arm64")
+    set(OPENSSL_OS darwin64-arm64-cc)
+  else()
+    set(OPENSSL_OS darwin64-x86_64-cc)
+  endif()
+else()
+  set(OPENSSL_OS linux-x86_64)
+endif()
+
+
 set(UNIX_CFGCMD ${DEPS_BUILD_DIR}/src/openssl/Configure --release
   --prefix=${DEPS_INSTALL_DIR} --openssldir=/usr/lib/ssl
-  --libdir=${DEPS_INSTALL_DIR}/lib no-shared no-tests linux-x86_64
+  --libdir=${DEPS_INSTALL_DIR}/lib no-shared no-tests ${OPENSSL_OS}
   CC=${DEPS_C_COMPILER} CPPFLAGS=-fPIC MAKE=${MAKE_PRG})
 
 ExternalProject_Add(openssl
